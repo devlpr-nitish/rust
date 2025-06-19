07871a69b8f214a2f11589f98421fa2f814e3798
Nitish <nk3227610@gmail.com>
2025-06-20 04:12:21 +0530

    guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!");
+
+    let rand_num = rand::thread_rng().gen_range(1, 101); 
+
+    loop {
+        println!("{}", "Enter your number..".yellow());
+
+        let mut num = String::new();
+
+        io::stdin().read_line(&mut num).expect("Error while taking input");
+
+        // converting string to number
+        let num:i32 = match num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match num.cmp(&rand_num) {
+            Ordering::Greater => println!("{}", "you guess the greater number".red()),
+            Ordering::Less => println!("{}", "you guess the lesser number".red()),
+            Ordering::Equal => {
+                println!("{}", "you won the game".green());
+                break;
+            }
+        }
+    }
+
 }
